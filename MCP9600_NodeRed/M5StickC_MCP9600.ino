5a6eef9e53d4fd798898f317f0d40c904c5d7058
shintaro <shintaro@shintaro-no-MacBook-Air.local>
2020-04-09 17:45:11 +0900

    node-ref file

---
+++ MCP9600_NodeRed/M5StickC_MCP9600.ino
@@ -0,0 +1,52 @@
+#include <M5StickC.h>
+
+void setup() {
+  M5.begin();
+  Wire.begin(0, 26);
+}
+
+void loop() {
+  byte error, address;
+  int nDevices;
+  M5.Lcd.setRotation(3);
+  M5.Lcd.setTextSize(4);
+//  Serial.println("Scanning... Wire");
+  nDevices = 0;
+  for (address = 1; address < 127; address++ )
+  {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      //Serial.print("I2C device found at address 0x");
+      if (address < 16)
+      //Serial.print("0");
+      //Serial.print(address, HEX);
+      Wire.write(0x00); 
+      Wire.endTransmission(false) ;
+      Wire.requestFrom(address, 2, true);
+      byte val1 = Wire.read();
+      byte val2 = Wire.read();
+      double temp = 0.0625*(val1*256 + val2);
+      Serial.println(temp);
+      M5.Lcd.setCursor(20, 20, 1);
+      M5.Lcd.print(temp);
+
+      nDevices++;
+    }
+    else if (error == 4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);  
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(1000);
+}
---
+++ MCP9600_NodeRed/README.md
@@ -0,0 +1,5 @@
+# M5StickC_MCP9600
+熱電対温度センサモジュールMCP9600をM5StickCに接続し、Node-REDでグラフを描画するコード  
+
+詳細は以下をご覧ください  
+https://shintarof.hatenablog.com/entry/2020/03/18/232444
